c8dd8f4e863e933781e5231d0df7f15077518ea0
Dev Karan Pradhan <dvpradhan312@gmail.com>
2020-09-14 17:09:01 +0530

    formatting

--- src/components/DropdownMenuComponent.js
+++ src/components/DropdownMenuComponent.js
@@ -4,14 +4,16 @@ import DropdownItemComponent from './DropdownItemComponent';
 import { DropdownMenu } from 'reactstrap';
 import DropdownItemWrapper from './DropdownItemWrapper';
 import { logoutRequest } from '../actions/formActions';
+import { Link } from 'react-router-dom';
 
 const DropdownMenuComponent = () => {
   const dispatch = useDispatch();
   const { userDetails } = useSelector((state) => state.loginReducer);
-
+  let ele1 = <DropdownItemComponent option="Profile" />;
   return (
     <DropdownMenu right>
-      <DropdownItemComponent option="Profile" />
+      <Link to="/profile">{ele1}</Link>
+      
       <DropdownItemWrapper
         option="Logout"
         onClick={() => {
