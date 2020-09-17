dd506600d11fa20fb82ab11ce17cf8bad746620f
Dev Karan Pradhan <dvpradhan312@gmail.com>
2020-09-17 17:34:35 +0530

    updated addproductapi

--- src/apis/addProductAPI.js
+++ src/apis/addProductAPI.js
@@ -18,28 +18,30 @@ const addProduct = ({
   let product_title = productTitle,
     product_price = productPrice,
     category_id = categoryId,
-    image_url = imageUrl;
+    image_url = [];
+  image_url.push(imageUrl);
   const headers = {
     Accept: 'application/vnd.e-commerce.v1',
     Token: token
   };
+  const data = {
+    product_title: product_title,
+    description: description,
+    product_price: parseFloat(product_price),
+    discount: parseFloat(discount),
+    tax: parseFloat(tax),
+    stock: parseInt(stock),
+    brand: brand,
+    category_id: parseInt(category_id),
+    category: category,
+    color: color,
+    size: size,
+    image_url: image_url
+  };
   return apiHelper(
     'post',
     `${process.env.REACT_APP_SERVER_URL}createProduct`,
-    {
-      product_title,
-      description,
-      product_price,
-      discount,
-      tax,
-      stock,
-      brand,
-      category_id,
-      category,
-      color,
-      size,
-      image_url
-    },
+    data,
     headers
   );
 };
