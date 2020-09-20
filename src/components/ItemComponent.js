b0430fb6402fdf2aff7702147d1f4552ab708c62
Dev Karan Pradhan <dvpradhan312@gmail.com>
2020-09-20 14:56:49 +0530

    added min max validation

--- src/components/ItemComponent.js
+++ src/components/ItemComponent.js
@@ -5,7 +5,8 @@ import FormInput from './FormInput';
 import ButtonWrapper from './ButtonWrapper';
 import { setMinMax, applyPriceFilter, applyFilters } from '../actions/productListActions';
 import { useDispatch } from 'react-redux';
-import { Form } from 'reactstrap';
+import { Form, FormGroup } from 'reactstrap';
+import FormFeedbackWrapper from './FormFeedbackWrapper';
 
 const ItemComponent = ({ label, index, toggle, setLabel, setProducts, products, labels }) => {
   const [filterby, setFilterby] = useState('');
@@ -26,22 +27,48 @@ const ItemComponent = ({ label, index, toggle, setLabel, setProducts, products, 
   }
   const [min, setMin] = useState(0);
   const [max, setMax] = useState(0);
+  const [minError, setMinError] = useState(null);
+  const [maxError, setMaxError] = useState(null);
 
   const setSublabel = (props) => {
     setFilterby(props);
   };
 
   const setPrice = () => {
+    setMinError(null);
+    setMaxError(null);
+    if(parseFloat(min)>parseFloat(max)) {
+      setMaxError("max should be greater than min");
+    }
+    if(parseFloat(min) < 0) {
+      setMinError("Must be positive");
+    }
+    if(parseFloat(max) < 0) {
+      setMaxError("Must be positive");
+    }
+    var reg = /^-?\d+$/;
+    if(!reg.test(min))
+    {
+      setMinError("min must be number");
+    }
+    if(!reg.test(max))
+    {
+      setMaxError("max must be number");
+    }
     if(min === ""){
       setMin("0");
+      setMinError(null);
     }
     if(max === ""){
       setMax("500,000");
+      setMaxError(null);
     }
-    if(min<0){
-      
-    }
+
     dispatch(setMinMax(min, max));
+    if(minError === null && maxError === null) {
+      dispatch(applyFilters());
+      dispatch(applyPriceFilter());
+    }
   }
 
   if(label.label !== "Price"){
@@ -88,12 +115,19 @@ const ItemComponent = ({ label, index, toggle, setLabel, setProducts, products, 
         </div>
         <div className="sublabel">
           <Form>
-          <FormInput type="text" placeholder="Min 0" value={min} onChange={(e) => setMin(e.target.value)}/>
-          To
-          <FormInput type="text" placeholder="Max 500,000" value={max} onChange={(e) => setMax(e.target.value)}/>
-          <ButtonWrapper buttonText="Find" color="info" onClick={() => {setPrice();
-          dispatch(applyFilters());
-          dispatch(applyPriceFilter());}}/>
+            <FormGroup>
+              <FormInput type="text" placeholder="Min 0" value={min} onChange={(e) => setMin(e.target.value)} invalid={minError !== null ? true : false}/>
+              <FormFeedbackWrapper message={minError}/>
+            </FormGroup>  
+            To
+            <FormGroup>
+              <FormInput type="text" placeholder="Max 500,000" value={max} onChange={(e) => setMax(e.target.value)} invalid={maxError !== null ? true : false}/>
+              <FormFeedbackWrapper message={maxError}/>
+            </FormGroup>
+            <ButtonWrapper buttonText="Find" color="info" onClick={() => {
+              setPrice();
+            }}
+            />
           </Form>
         </div>
       </div>
